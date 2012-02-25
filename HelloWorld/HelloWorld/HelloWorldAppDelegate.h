e0b09520306d0e25565ca642a7412c63a6dabb2c
wayliu mbp <wayliu@live.com>
2012-02-25 17:30:51 +0800

    hello world iOS

---
+++ HelloWorld/HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by 伟 刘 on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
